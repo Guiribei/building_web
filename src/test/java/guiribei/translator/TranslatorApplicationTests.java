e046c5b8f46f61dff97ab82b2366195e8edb04f0
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-01-21 20:45:32 -0300

    feat: initial setup

---
+++ src/test/java/guiribei/translator/TranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package guiribei.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
